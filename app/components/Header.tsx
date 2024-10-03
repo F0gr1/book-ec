57e0d066a1629586310247234462baccfa8e32a8
F0gr1 <ishigami0314@icloud.com>
2024-10-04 01:07:19 +0900

    complete

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,14 +1,22 @@
-"use client";
+/* eslint-disable @next/next/no-async-client-component */
+import { signOut} from "next-auth/react";
+import { User } from "../types/type";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { getServerSession } from "next-auth";
+import { nextAuthOptions } from "../lib/nexr-auth/options";
+const Header = async() => {
 
-const Header = () => {
+  const session = await getServerSession(nextAuthOptions);
+  const user: User = session?.user as User;
+
+
   return (
     <header className="bg-slate-600 text-gray-100 shadow-lg">
       <nav className="flex items-center justify-between p-4">
         <Link href={"/"} className="text-xl font-bold">
-          Book Commerce
+          Book EC
         </Link>
         <div className="flex items-center gap-1">
           <Link
@@ -18,18 +26,20 @@ const Header = () => {
             ホーム
           </Link>
           <Link
-            href="/login"
+            href={user ? "/profile" : "/api/auth/signIn"}
             className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
           >
-            ログイン
+            {user ?"プロフィール" :"ログイン"}
           </Link>
+
+          {user ? <Link href={"api/auth/signout"} className="text-gray-300 hover:text-white px-3 py-2 rounded-md text-sm font-medium">ログアウト</Link> : ""}
 
           <Link href={`/profile`}>
             <Image
               width={50}
               height={50}
               alt="profile_icon"
-              src={"/default_icon.png"}
+              src={user?.image ||"/default_icon.png"}
             />
           </Link>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,41 @@
 
-export default function Home() {
+import { getServerSession } from "next-auth";
+import Book from "./components/book";
+import { getAllBooks } from "./lib/microcms/client";
+import { BookType, Purchase, User } from "./types/type";
+import { nextAuthOptions } from "./lib/nexr-auth/options";
+
+// eslint-disable-next-line @next/next/no-async-client-component
+export default async function Home() {
+
+  const {contents} = await getAllBooks();
+  const session = await getServerSession(nextAuthOptions);
+  const user: User = session?.user as User;
+  
+  let purchaseBookdIds: string[];
+
+  if(user){
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`,
+        { cache : "no-cache"} //SSR
+    );
+
+    const purhcasesData = await response.json();
+
+    purchaseBookdIds = purhcasesData.map(
+       (purhcasesBook: Purchase) => purhcasesBook.bookId
+    )
+  }
   return (
-    <div></div>
+    <>
+      <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
+        <h2 className="text-center w-full font-bold text-3xl mb-2">
+          Book Commerce
+        </h2>
+        {contents.map((book: BookType) => (
+          <Book key={book.id} book={book} isPurchased={purchaseBookdIds.includes(book.id)}/>
+        ))}
+      </main>
+    </>
   );
 }
